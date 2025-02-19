2c359d11068f00905d1781e5c5b9a665735058c7
VikingVik123 <vodimmegwa@gmail.com>
2025-02-19 16:31:42 +0100

    updated main file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.router import api_router
 import os
 
-api_key = os.getenv("api_key")
+api_key = "zuq/hiBZxcaGPHYblr30kw==klbwkoKYvSMnDUqi"
 if not api_key:
     raise ValueError("API Key is missing. Set the 'api_key' environment variable.")
 
