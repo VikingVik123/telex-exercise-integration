e44bed79ccc2b3ff13092904db91ffa2f4f53df4
VikingVik123 <vodimmegwa@gmail.com>
2025-02-22 08:42:37 +0100

    updated integration json

--- integration.py
+++ integration.py
@@ -8,7 +8,7 @@ int_json = {
       "app_name": "Daily WorkOut",
       "app_description": "Fetches workout data from an exercise API and sends it to Slack via Telex.",
       "app_logo": "https://images.app.goo.gl/XTLJhuQC53HMV6HY8",
-      "app_url": "https://3.85.32.124/",
+      "app_url": "https://telex-integration-evq8.onrender.com/",
       "background_color": "#000000"
     },
     "is_active": True,
@@ -17,7 +17,7 @@ int_json = {
     "key_features": [
       "Fetches workout data from an exercise API."
     ],
-    "website": "https://3.85.32.124/exe",
+    "website": "https://telex-integration-evq8.onrender.com/exe",
     "author": "Victor",
     "settings": [
       {
